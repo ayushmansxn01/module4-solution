405b36097c0a25ae9e96aea9780a52e34fe15eff
ayushmansxn01 <ayushmansxn01@gmail.com>
2021-09-07 18:47:51 +0530

    module4 solution

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log("Hello Yaakov");
+console.log("Good Bye John");
+console.log("Good Bye Jen");
+console.log("Good Bye Jason");
+console.log("Hello Paul");
+console.log("Hello Frank");
+console.log("Hello Paula");
+console.log("Hello Laura");
+console.log("Good Bye Jim");
